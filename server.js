ef49b2d1965c80702335a8a5a5eb1580b2ee2685
DimaSavchenko <dimasauchanka@gmail.com>
2017-12-05 17:20:01 +0300

    test jsreport-contrib-mongodb

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
@@ -8,13 +9,50 @@ app.get('/', function (req, res) {
 var reportingApp = express();
 app.use('/reporting', reportingApp);
 
-var server = app.listen(3000);
+var server = app.listen(8000);
 
 var jsreport = require('jsreport')({
   express: { app :reportingApp, server: server },
   appPath: "/reporting"
 });
 
-jsreport.init().catch(function (e) {
-  console.error(e);
-});
+function beforeRender(done) {
+  var MongoClient = require('mongodb').MongoClient;
+  MongoClient.connect('mongodb://127.0.0.1:27017/phoenix-api', function(err, db) {
+    // db.listCollections().toArray(function(err, collInfos) {
+    //   // collInfos is an array of collection info objects that look like:
+    //   console.log(collInfos);
+    // });
+    var collection = db.collection('product').find().toArray(function(err, results) {
+      // console.log(request);
+      // if (err)
+      //   return done(err);
+
+      // request.data = { product: results };
+      Object.assign(request.data, { countries: results })
+      // done();
+    });
+  });
+}
+
+beforeRender();
+
+jsreport.init().then(function () {
+
+
+  jsreport.render({
+    template: {
+      content: '<h1>Hello {{:foo}}</h1>',
+      engine: 'handlebars',
+      recipe: 'phantom-pdf'
+    },
+    data: {
+      foo: "world"
+    }
+  }).then(function(resp) {
+    // write report buffer to a file
+    fs.writeFileSync('report.pdf', resp.content)
+  });
+}).catch(function(e) {
+  console.log(e)
+})
