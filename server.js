9d6eea6b09a42cdf14f260a3f5cec977326c67e4
DimaSavchenko <dimasauchanka@gmail.com>
2017-12-07 14:43:33 +0300

    render template, jsreport-contrib-mongodb

--- server.js
+++ server.js
@@ -12,47 +12,47 @@ app.use('/reporting', reportingApp);
 var server = app.listen(8000);
 
 var jsreport = require('jsreport')({
-  express: { app :reportingApp, server: server },
+  express: { app: reportingApp, server: server },
   appPath: "/reporting"
 });
 
+var request = {};
+
 function beforeRender(done) {
   var MongoClient = require('mongodb').MongoClient;
-  MongoClient.connect('mongodb://127.0.0.1:27017/phoenix-api', function(err, db) {
-    // db.listCollections().toArray(function(err, collInfos) {
-    //   // collInfos is an array of collection info objects that look like:
-    //   console.log(collInfos);
-    // });
-    var collection = db.collection('product').find().toArray(function(err, results) {
-      // console.log(request);
-      // if (err)
-      //   return done(err);
+  MongoClient.connect('mongodb://127.0.0.1:27017/phoenix-api', function (err, db) {
+    var collection = db.collection('product').find().toArray(function (err, results) {
+      if (err)
+        return done(err);
 
-      // request.data = { product: results };
-      Object.assign(request.data, { countries: results })
-      // done();
+      request.data = { product: results[0] };
     });
   });
 }
 
 beforeRender();
 
+request.template = {
+  content: `
+    <hi>{{product.name}}</h1>
+    <ul>
+      {{#each product.features}}
+        <li>{{this.name}}</li>
+      {{/each}}
+    </ul>
+  `,
+  engine: 'handlebars',
+  recipe: 'phantom-pdf'
+};
+
 jsreport.init().then(function () {
 
 
-  jsreport.render({
-    template: {
-      content: '<h1>Hello {{:foo}}</h1>',
-      engine: 'handlebars',
-      recipe: 'phantom-pdf'
-    },
-    data: {
-      foo: "world"
-    }
-  }).then(function(resp) {
+  jsreport.render(request).then(function (resp) {
     // write report buffer to a file
     fs.writeFileSync('report.pdf', resp.content)
   });
-}).catch(function(e) {
+}).catch(function (e) {
   console.log(e)
 })
+
